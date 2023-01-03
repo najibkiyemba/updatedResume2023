e6084bc8b3d781dd321af6ce3a90cdecc50b1b73
Najib Kaur <najibkaur@Najibs-MacBook-Pro.local>
2023-01-03 13:03:05 -0800

    Setup for development is done

---
+++ src/components/optionalParts/trainingCertificationLicenses/TrainingCertification.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./trainingCertification.css";
+
+
+const TrainingCertification = () => {
+
+    return (
+        <div className="trainingCerti">
+            
+        </div>
+    )
+};
+
+export default TrainingCertification;
