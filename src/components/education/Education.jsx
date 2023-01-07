14b9ea3f4b4ed7b095f2c1fb41d45bad83cbd9e2
Najib Kaur <najibkaur@Najibs-MacBook-Pro.local>
2023-01-07 01:55:10 -0800

    education completed component

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -8,7 +8,32 @@ const Education = () => {
 
     return (
         <div className="education">
-            <h1>Education</h1>
+            <h1>EDUCATION</h1>
+            <div className="highSchool">
+                <h4>High School Diploma</h4>
+                <p>Excel Center South Bend, Idiana.</p>
+                <p>Aug 2021</p>
+                <div className="roles">
+                    <ul>
+                        <li>Archived Safety & Health Fundamentals Certificate Through OSHA Online Education Training.</li>
+                        <li>Passed all science classes with A+.</li>
+                        <li>Lead a team of about 7 - 10 colleagues to solve math(algebra) problems.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="highSchool">
+                <h4>Associate of Applied Science Degree.</h4>
+                <p>Ivy Tech Community College USA</p>
+                <p>Expected 2024</p>
+                <div className="roles">
+                    <ul>
+                        <li>Completed and passed web development classes using HTML and CSS.</li>
+                        <li>Wrote about 3-5 eassays using more than 2000 words in each.</li>
+                        <li>Wrote a paper about Internet of Things(IOT)</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
